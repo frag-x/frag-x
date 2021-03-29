e267f2e37b5bc16c323a60ca52f78000c4dfc81a
Momo <momo@Momo-PC.localdomain>
2021-03-28 20:22:03 -0700

    asterix

--- network/client/client.cpp
+++ network/client/client.cpp
@@ -6,7 +6,7 @@
 
 static ChatScreen chatScreen;
 
-void SendPacket(ENetPeer peer,const char* data)
+void SendPacket(ENetPeer* peer,const char* data)
 {
   ENetPacket* packet = enet_packet_create(data,strlen(data) + 1,ENET_PACKET_FLAG_RELIABLE);
   enet_peer_send(peer,0,packet);

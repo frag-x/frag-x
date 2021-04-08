df7164e883bfabc07021f00b18507b9e1d191f74
cuppajoeman <ccn@cuppajoeman.com>
2021-04-08 15:21:41 -0400

    add in text representation of input

--- love/fx/player.lua
+++ love/fx/player.lua
@@ -1,34 +1,48 @@
 --! file: player.lua
 Player = Object:extend()
 
-function Player:new()
-    self.x = 300
-    self.y = 20
-    self.body = love
-    self.speed = 500
-    self.radius = 20
+function Player:new(world, scr_w, scr_h, radius, speed)
+
+    -- Give the player physics
+
+    self.body = love.physics.newBody(world, scr_w/2, scr_h/2, "dynamic") 
+    self.shape = love.physics.newCircleShape( 20) 
+    self.fixture = love.physics.newFixture(self.body, self.shape, 1) 
+    self.fixture:setRestitution(0.3) --let the ball bounce
+
+    -- Players settings
+
+    self.speed = speed
+    self.radius = radius
     self.resolution = 100
-    self.sensitivity = 0.001
+    self.sensitivity = 0.011
     self.mouseX = 0
     self.rotationAngle = 0
-    self.aimLength = 60
+    self.aimLength = 150
 end
 
 function Player:update(dt)
 
   --! key input
   
-  x_dir = bool_to_number(love.keyboard.isDown("right")) - bool_to_number(love.keyboard.isDown("left")) 
-  y_dir = bool_to_number(love.keyboard.isDown("down")) - bool_to_number(love.keyboard.isDown("up")) 
+  self.x_dir = bool_to_number(love.keyboard.isDown("d")) - bool_to_number(love.keyboard.isDown("a")) 
+  self.y_dir = bool_to_number(love.keyboard.isDown("s")) - bool_to_number(love.keyboard.isDown("w")) 
 
-
-  self.x = self.x + x_dir * self.speed * dt
-  self.y = self.y + y_dir * self.speed * dt
-
-  --! self.rotationAngle = self.rotationAngle + x_change * self.sensitivity
-  print(self.rotationAngle)
+  self.body:applyForce(self.speed * self.x_dir, self.speed * self.y_dir)
 end
 
+function Player:inputStateAsText()
+  -- format:
+  -- |x_dir|y_dir|rotationAngle
+  data = "|"
+  data = data .. tostring(self.x_dir)
+  data = data .. "|"
+  data = data .. tostring(self.y_dir)
+  data = data .. "|"
+  data = data .. tostring(self.rotationAngle)
+  data = data .. "|"
+  return data
+end
 
 function Player:mousemoved(x, y, dx, dy)
   self.rotationAngle = self.rotationAngle + dx * self.sensitivity
@@ -43,3 +57,4 @@ function Player:draw()
     love.graphics.circle("fill",self.x, self.y, self.radius, self.resolution)
     love.graphics.line(self.x, self.y, self.x + math.cos(self.rotationAngle) * self.aimLength , self.y + math.sin(self.rotationAngle) * self.aimLength)
 end
+

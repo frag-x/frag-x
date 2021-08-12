2c2194a54ba8363d1b31b1891e2338975d045746
Norma Cassidy <normacassidy@Ivans-MacBook-Air.local>
2021-08-11 23:22:04 -0300

    Refactor client

--- client.py
+++ client.py
@@ -59,9 +59,7 @@ def parse_args():
                         help='game map')
     return parser.parse_args()
 
-def run_client(args):
-    # logging.basicConfig(level=logging.INFO)
-
+def initialize_pygame():
     ## initialize pygame and create window
     pygame.init()
     pygame.mixer.init()  ## For sound
@@ -87,18 +85,24 @@ def run_client(args):
         dev_constants.SCREEN_FOR_DEBUGGING = screen
 
     pygame.display.set_caption(game_engine_constants.GAME_TITLE)
-    clock = pygame.time.Clock()  # For syncing the FPS
+    clock = pygame.time.Clock()
 
-    # Initialize network
-    network = Network(game_engine_constants.DEFAULT_IP, args.port, game_engine_constants.BUF_SIZE)
+    pygame.mouse.set_visible(False)
+    pygame.event.set_grab(True)
+
+    return screen, font, clock
+
+def initialize_network(ip_address, port):
+    network = Network(ip_address, port, game_engine_constants.BUF_SIZE)
     player_id = network.connect()
 
     print(f"You are player {player_id}")
 
-    # initially we don't know what our id is we only get it back from the server so we need to do
-    # a type of responce thing..
+    return network, player_id
+
+def initialize_player(network, player_id):
     rand_color = random.choices(range(256), k=3)
-    curr_player = ClientPlayer(
+    return ClientPlayer(
         game_engine_constants.ORIGIN,
         game_engine_constants.TILE_SIZE,
         game_engine_constants.TILE_SIZE,
@@ -109,203 +113,199 @@ def run_client(args):
         network,
     )
 
-    client_game_manager = ClientGameManager(screen, font, f'{game_engine_constants.MAP_PREFIX}{args.map}', curr_player, network)
-
-    ## group all the sprites together for ease of update
-    # TODO REMOVE THIS AND JUST USE A SET
+def initialize_client_game_manager(network, screen, font, player, player_id):
+    # TODO: this should probably just be a simple call to ClientGameManager.__init__
+    client_game_manager = ClientGameManager(screen, font, f'{game_engine_constants.MAP_PREFIX}{args.map}', player, network)
     client_game_manager.all_sprites = pygame.sprite.Group()
+    client_game_manager.all_sprites.add(player)
+    client_game_manager.id_to_player[player_id] = player
+    return client_game_manager
 
-    client_game_manager.all_sprites.add(curr_player)
+def update(client_game_manager, delta_time, player, events):
+    message_to_send = ""
 
-    client_game_manager.id_to_player[player_id] = curr_player
+    if not client_game_manager.is_typing:
+        if helpers.started_typing(events):  # only check if they pressd when not typing
+            client_game_manager.is_typing = True
+            just_started = True
+    else:
+        if helpers.ended_typing_and_do_action(
+            events
+        ):  # they are typing and then press return
+            client_game_manager.is_typing = False
+            # DO ACTION
+            message = client_game_manager.user_text_box.text
+            if commands.is_command(message):
+                full_command = message
+                successful = (
+                    client_game_manager.client_command_runner.attempt_run_command(
+                        full_command
+                    )
+                )
+                if successful:
+                    print("command went through!")
+                else:
+                    print("command failed")
+            else:
+                # then we're dealing with a normal chat message
+                message_to_send = message
+
+            # print(f"sending {client_game_manager.user_text_box.text}")
+            client_game_manager.user_text_box.text = ""
+        elif helpers.ended_typing_and_do_nothing(events):
+            client_game_manager.is_typing = False
+            client_game_manager.user_text_box.text = ""
+
+    if client_game_manager.is_typing and not just_started:
+        client_game_manager.user_text_box.update(
+            events
+        )  # update the textbox if they're typing
+
+    just_started = False
+
+    # Note: This sends the users inputs to the server
+    client_game_manager.all_sprites.update(events, delta_time)
+
+    player.send_inputs(
+        delta_time, client_game_manager.is_typing, message_to_send
+    )
+
+def render(client_game_manager, delta_time, player, screen, font):
+    screen.fill(pygame.color.THECOLORS["black"])
+
+    client_game_manager.player_data_lock.acquire()
+    for row in client_game_manager.partitioned_map_grid.partitioned_map:
+        for partition in row:
+            pygame.draw.rect(
+                screen,
+                pygame.color.THECOLORS["gold"],
+                partition.rect.move(player.camera_v),
+                width=1,
+            )
+
+            for wall in partition.walls:
+                pygame.draw.rect(
+                    screen, wall.color, wall.rect.move(player.camera_v)
+                )
+
+            for b_wall in partition.bounding_walls:
+                pygame.draw.rect(
+                    screen, b_wall.color, b_wall.rect.move(player.camera_v)
+                )
+    client_game_manager.player_data_lock.release()
+
+    if dev_constants.DEBUGGING_INTERSECTIONS:
+        for hit_v in dev_constants.INTERSECTIONS_FOR_DEBUGGING:
+            pygame.draw.circle(
+                dev_constants.SCREEN_FOR_DEBUGGING,
+                pygame.color.THECOLORS["purple"],
+                hit_v + player.camera_v,
+                3,
+            )
+
+        for partition in dev_constants.INTERSECTED_PARTITIONS_FOR_DEBUGGING:
+            pygame.draw.rect(
+                screen,
+                pygame.color.THECOLORS["blueviolet"],
+                partition.rect.move(player.camera_v),
+                width=1,
+            )
+
+        for point_v in dev_constants.INTERSECTED_PARTITION_PATCH_MARKERS:
+            pygame.draw.circle(
+                dev_constants.SCREEN_FOR_DEBUGGING,
+                pygame.color.THECOLORS["red"],
+                point_v + player.camera_v,
+                3,
+            )
+
+        for point_v in dev_constants.INTERSECTED_PARTITION_SEAMS_FOR_DEBUGGING:
+            pygame.draw.circle(
+                dev_constants.SCREEN_FOR_DEBUGGING,
+                pygame.color.THECOLORS["yellow"],
+                point_v + player.camera_v,
+                3,
+            )
+
+        for beam in dev_constants.BEAMS_FOR_DEBUGGING:
+            pygame.draw.line(
+                dev_constants.SCREEN_FOR_DEBUGGING,
+                pygame.color.THECOLORS["green"],
+                beam.start_point + player.camera_v,
+                beam.end_point + player.camera_v,
+            )
+
+    client_game_manager.draw_projectiles(player.camera_v)
+    client_game_manager.draw_beams(player.camera_v)
+
+    # A drawing is based on a single network message from the server.
+    # The reason why it looks like we have shifted tiles is that we received a message in the middle, so this needs to be locked.
+    # instead of actually simulating its movement that way it seems more solid
+    for sprite in client_game_manager.all_sprites:
+        # Add the player's camera offset to the coords of all sprites.
+        screen.blit(sprite.image, sprite.rect.topleft + player.camera_v)
+
+    # PLAYER PROPERTIES START
+
+    font_color = pygame.color.THECOLORS["brown3"]
+
+    pos = font.render(str(player.pos), False, font_color)
+    aim_angle_str = (
+        str(9 - math.floor(player.rotation_angle / math.tau * 10)) + "/" + str(10)
+    )
+    angle = font.render(aim_angle_str + "τ", False, font_color)
+
+    screen.blit(pos, (0, 25))
+    screen.blit(angle, (0, 50))
+
+    # PLAYER PROPERTIES END
+
+    # TEXT BOX START
+
+    client_game_manager.user_chat_box.update_message_times(delta_time)
+
+    client_game_manager.user_chat_box.draw_messages()
+    client_game_manager.user_text_box.render_text()
+
+    utb_width, utb_height = client_game_manager.user_text_box.image.get_size()
+
+    screen.blit(
+        client_game_manager.user_text_box.image,
+        (
+            game_engine_constants.WIDTH
+            - (utb_width + 2 * client_game_manager.user_text_box.border_thickness),
+            game_engine_constants.HEIGHT
+            - (utb_height + 2 * client_game_manager.user_text_box.border_thickness),
+        ),
+    )
+
+def run_client(args):
+    screen, font, clock = initialize_pygame()
+
+    network, player_id = initialize_network(args.ip_address, args.port)
+
+    player = initialize_player(network, player_id)
+
+    client_game_manager = initialize_client_game_manager(network, screen, font, player, player_id)
 
     t = Thread(target=game_state_watcher, args=(client_game_manager, network))
     t.start()
 
-    # Game loop
     running = True
-    ticks_from_previous_iteration = 0
-
-    # Initialization
-    pygame.mouse.set_visible(False)
-    pygame.event.set_grab(True)
-
     while running:
         # 1 Process input/events
-        clock.tick(game_engine_constants.FPS)  ## will make the loop run at the same speed all the time
+        delta_time = clock.tick(game_engine_constants.FPS) ## will make the loop run at the same speed all the time
+        delta_time /= 1000
 
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 running = False
 
-        # 2 Update
+        update(client_game_manager, delta_time, player, events)
 
-        t = pygame.time.get_ticks()
-        # deltaTime in seconds.
-        delta_time = (t - ticks_from_previous_iteration) / 1000.0
-        ticks_from_previous_iteration = t
-
-        message_to_send = ""
-
-        if not client_game_manager.is_typing:
-            if helpers.started_typing(events):  # only check if they pressd when not typing
-                client_game_manager.is_typing = True
-                just_started = True
-        else:
-            if helpers.ended_typing_and_do_action(
-                events
-            ):  # they are typing and then press return
-                client_game_manager.is_typing = False
-                # DO ACTION
-                message = client_game_manager.user_text_box.text
-                if commands.is_command(message):
-                    full_command = message
-                    successful = (
-                        client_game_manager.client_command_runner.attempt_run_command(
-                            full_command
-                        )
-                    )
-                    if successful:
-                        print("command went through!")
-                    else:
-                        print("command failed")
-                else:
-                    # then we're dealing with a normal chat message
-                    message_to_send = message
-
-                # print(f"sending {client_game_manager.user_text_box.text}")
-                client_game_manager.user_text_box.text = ""
-            elif helpers.ended_typing_and_do_nothing(events):
-                client_game_manager.is_typing = False
-                client_game_manager.user_text_box.text = ""
-
-        if client_game_manager.is_typing and not just_started:
-            client_game_manager.user_text_box.update(
-                events
-            )  # update the textbox if they're typing
-
-        just_started = False
-
-        # Note: This sends the users inputs to the server
-        client_game_manager.all_sprites.update(events, delta_time)
-
-        curr_player.send_inputs(
-            delta_time, client_game_manager.is_typing, message_to_send
-        )
-
-        # 3 Draw/render
-        screen.fill(pygame.color.THECOLORS["black"])
-
-        client_game_manager.player_data_lock.acquire()
-        for row in client_game_manager.partitioned_map_grid.partitioned_map:
-            for partition in row:
-                pygame.draw.rect(
-                    screen,
-                    pygame.color.THECOLORS["gold"],
-                    partition.rect.move(curr_player.camera_v),
-                    width=1,
-                )
-
-                for wall in partition.walls:
-                    pygame.draw.rect(
-                        screen, wall.color, wall.rect.move(curr_player.camera_v)
-                    )
-
-                for b_wall in partition.bounding_walls:
-                    pygame.draw.rect(
-                        screen, b_wall.color, b_wall.rect.move(curr_player.camera_v)
-                    )
-        client_game_manager.player_data_lock.release()
-
-        if dev_constants.DEBUGGING_INTERSECTIONS:
-            for hit_v in dev_constants.INTERSECTIONS_FOR_DEBUGGING:
-                pygame.draw.circle(
-                    dev_constants.SCREEN_FOR_DEBUGGING,
-                    pygame.color.THECOLORS["purple"],
-                    hit_v + curr_player.camera_v,
-                    3,
-                )
-
-            for partition in dev_constants.INTERSECTED_PARTITIONS_FOR_DEBUGGING:
-                pygame.draw.rect(
-                    screen,
-                    pygame.color.THECOLORS["blueviolet"],
-                    partition.rect.move(curr_player.camera_v),
-                    width=1,
-                )
-
-            for point_v in dev_constants.INTERSECTED_PARTITION_PATCH_MARKERS:
-                pygame.draw.circle(
-                    dev_constants.SCREEN_FOR_DEBUGGING,
-                    pygame.color.THECOLORS["red"],
-                    point_v + curr_player.camera_v,
-                    3,
-                )
-
-            for point_v in dev_constants.INTERSECTED_PARTITION_SEAMS_FOR_DEBUGGING:
-                pygame.draw.circle(
-                    dev_constants.SCREEN_FOR_DEBUGGING,
-                    pygame.color.THECOLORS["yellow"],
-                    point_v + curr_player.camera_v,
-                    3,
-                )
-
-            for beam in dev_constants.BEAMS_FOR_DEBUGGING:
-                pygame.draw.line(
-                    dev_constants.SCREEN_FOR_DEBUGGING,
-                    pygame.color.THECOLORS["green"],
-                    beam.start_point + curr_player.camera_v,
-                    beam.end_point + curr_player.camera_v,
-                )
-
-        client_game_manager.draw_projectiles(curr_player.camera_v)
-        client_game_manager.draw_beams(curr_player.camera_v)
-
-        # A drawing is based on a single network message from the server.
-        # The reason why it looks like we have shifted tiles is that we received a message in the middle, so this needs to be locked.
-        # instead of actually simulating its movement that way it seems more solid
-        for sprite in client_game_manager.all_sprites:
-            # Add the player's camera offset to the coords of all sprites.
-            screen.blit(sprite.image, sprite.rect.topleft + curr_player.camera_v)
-
-        # PLAYER PROPERTIES START
-
-        font_color = pygame.color.THECOLORS["brown3"]
-
-        pos = font.render(str(curr_player.pos), False, font_color)
-        aim_angle_str = (
-            str(9 - math.floor(curr_player.rotation_angle / math.tau * 10)) + "/" + str(10)
-        )
-        angle = font.render(aim_angle_str + "τ", False, font_color)
-
-        screen.blit(pos, (0, 25))
-        screen.blit(angle, (0, 50))
-
-        # PLAYER PROPERTIES END
-
-        # TEXT BOX START
-
-        client_game_manager.user_chat_box.update_message_times(delta_time)
-
-        client_game_manager.user_chat_box.draw_messages()
-        client_game_manager.user_text_box.render_text()
-
-        utb_width, utb_height = client_game_manager.user_text_box.image.get_size()
-
-        screen.blit(
-            client_game_manager.user_text_box.image,
-            (
-                game_engine_constants.WIDTH
-                - (utb_width + 2 * client_game_manager.user_text_box.border_thickness),
-                game_engine_constants.HEIGHT
-                - (utb_height + 2 * client_game_manager.user_text_box.border_thickness),
-            ),
-        )
-
-        # TEXT BOX END
-
-        ## Done after drawing everything to the screen
+        render(client_game_manager, delta_time, player, screen, font)
+        
         pygame.display.flip()
 
     pygame.quit()

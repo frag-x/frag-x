1083219f0f03c9317e972c62fb8779272e4b0d93
Norma Cassidy <normacassidy@Ivans-MacBook-Air.local>
2021-08-12 23:20:56 -0300

    Add things

---
+++ comms/network.py
@@ -0,0 +1,26 @@
+from comms.message import Message
+import pickle
+import socket
+
+def _recv_exactly(socket: socket.socket, num_bytes: int) -> bytes:
+    data = b''
+    while len(data) < num_bytes:
+        chunk = socket.recv(num_bytes - len(data))
+        if not chunk:
+            raise BrokenPipeError('Socket shut down while receiving')
+        data += chunk # TODO this is quadratic, should join instead
+    return data
+
+def send(socket: socket.socket, message: Message):
+    bytes = pickle.dumps(message)
+    num_message_bytes = len(bytes).to_bytes(4, 'little')
+
+    socket.sendall(num_message_bytes)
+    socket.sendall(bytes)
+    
+def recv(socket: socket.socket) -> Message:
+    num_message_bytes_as_bytes = _recv_exactly(socket, 4)
+    num_message_bytes = int.from_bytes(num_message_bytes_as_bytes, 'little')
+
+    message_as_bytes = _recv_exactly(socket, num_message_bytes)
+    return pickle.loads(message_as_bytes)

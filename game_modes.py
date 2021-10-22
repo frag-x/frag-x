19320bc9d951d3fd1fd7caec8326c2c7810106a2
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2021-10-22 17:33:35 -0400

    Update game_modes.py

--- game_modes.py
+++ game_modes.py
@@ -15,6 +15,12 @@ class ConditionGameMode(GameMode):  # CTF
 
 
 class FirstToNFrags(ConditionGameMode):
-    def __init__(self, frags_to_win=1000):
+    def __init__(self, frags_to_win=15, respawn_time = 5):
         self.frags_to_win = frags_to_win
-        self.respawn_time = 5  # measured in seconds
+        self.respawn_time = respawn_time  # measured in seconds
+
+
+class InstakillGameMode(FirstToNFrags): #Instakill
+    def __init__(self, frags_to_win=15, respawn_time = 5):
+        super().__init__(frags_to_win=frags_to_win, respawn_time=respawn_time)
+
